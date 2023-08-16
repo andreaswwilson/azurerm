5e36033186b1a80b798275ff0deee8c6b5614d49
Andreas Wågø Wilson <AndreasWago.Wilson@skatteetaten.no>
2023-08-16 07:59:45 +0200

    Remove SPs

--- main.tf
+++ main.tf
@@ -17,37 +17,35 @@ resource "azurerm_storage_container" "tfstate" {
 }
 
 ########## Service principal ########## 
-data "azuread_client_config" "current" {}
 
-# Create Azure AD App -
-resource "azuread_application" "msp" {
-  display_name = "sp-msp"
-
-  required_resource_access {
-    resource_app_id = "00000003-0000-0000-c000-000000000000" # MS Graph app id.
-
-    resource_access {
-      id   = "18a4783c-866b-4cc7-a460-3d5e5662c884" # Application.ReadWrite.OwnedBy
-      type = "Scope"
-    }
-  }
-
-}
-
-# Create Service Principal
-resource "azuread_service_principal" "msp" {
-  application_id = azuread_application.msp.application_id
-  depends_on     = [azuread_application.msp]
-}
-
-# Create Azure AD App -
-resource "azuread_application" "sp1" {
-  display_name = "sp-sp1"
-  owners       = [azuread_service_principal.msp.object_id]
-}
-
-# Create Service Principal
-resource "azuread_service_principal" "sp1" {
-  application_id = azuread_application.msp.application_id
-  owners         = [azuread_service_principal.msp.object_id]
-}
+# # Create Azure AD App
+# resource "azuread_application" "msp" {
+#   display_name = "sp-msp"
+#
+#   required_resource_access {
+#     resource_app_id = "00000003-0000-0000-c000-000000000000" # MS Graph app id.
+#
+#     resource_access {
+#       id   = "18a4783c-866b-4cc7-a460-3d5e5662c884" # Application.ReadWrite.OwnedBy
+#       type = "Scope"
+#     }
+#   }
+#
+# }
+#
+# # Create Service Principal
+# resource "azuread_service_principal" "msp" {
+#   application_id = azuread_application.msp.application_id
+# }
+#
+# # Create Azure AD App
+# resource "azuread_application" "sp1" {
+#   display_name = "sp-sp1"
+#   owners       = [azuread_service_principal.msp.object_id]
+# }
+#
+# # Create Service Principal
+# resource "azuread_service_principal" "sp1" {
+#   application_id = azuread_application.msp.application_id
+#   owners         = [azuread_service_principal.msp.object_id]
+# }
