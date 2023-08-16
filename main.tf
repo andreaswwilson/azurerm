8d0fd6715de2aac4f127ff4ea1ec703adf2f1e9b
Andreas Wågø Wilson <AndreasWago.Wilson@skatteetaten.no>
2023-08-16 09:33:55 +0200

    Move SP here

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
+locals {
+  location = "Sweden Central"
+}
+
 resource "azurerm_resource_group" "sa" {
   name     = "rg-storage-account"
-  location = "Sweden Central"
+  location = local.location
 }
 
 resource "azurerm_storage_account" "sa" {
@@ -15,4 +19,36 @@ resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
   storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "private"
+}
+
+
+######################
+# Service Principals #
+######################
+
+# MSP
+resource "azuread_application" "msp" {
+  display_name = "sp-msp"
+
+  required_resource_access {
+    resource_app_id = "00000003-0000-0000-c000-000000000000" # MS Graph app id.
+
+    resource_access {
+      id   = "18a4783c-866b-4cc7-a460-3d5e5662c884" # Application.ReadWrite.OwnedBy
+      type = "Scope"
+    }
+  }
+}
+resource "azuread_service_principal" "msp" {
+  application_id = azuread_application.msp.application_id
+}
+
+# SP1
+resource "azuread_application" "sp1" {
+  display_name = "sp1"
+  owners       = [azuread_service_principal.msp.object_id]
+}
+resource "azuread_service_principal" "sp1" {
+  application_id = azuread_application.sp1.application_id
+  owners         = [azuread_service_principal.msp.object_id]
 }
