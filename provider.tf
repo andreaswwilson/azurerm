081d4ed1cb892e41ebceed53b46d7f54de3320ab
Andreas Wågø Wilson <AndreasWago.Wilson@skatteetaten.no>
2024-02-20 20:03:50 +0100

    use_oicd

--- provider.tf
+++ provider.tf
@@ -13,6 +13,7 @@ provider "azurerm" {
   subscription_id     = "cec8d728-7497-4f8c-b344-87b95fcc6e92"
   tenant_id           = "ad7e197d-b1f9-411a-8c1d-f7c87c21f636"
   storage_use_azuread = "true"
+  use_oidc            = true
 }
 
 terraform {
