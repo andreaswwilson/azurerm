a289fdad23149878d2e98589d5b662b590c1de12
Andreas Wågø Wilson <AndreasWago.Wilson@skatteetaten.no>
2024-02-20 20:17:13 +0100

    .

--- provider.tf
+++ provider.tf
@@ -12,14 +12,17 @@ provider "azurerm" {
   features {}
   subscription_id     = "cec8d728-7497-4f8c-b344-87b95fcc6e92"
   tenant_id           = "ad7e197d-b1f9-411a-8c1d-f7c87c21f636"
-  storage_use_azuread = "true"
+  storage_use_azuread = true
   use_oidc            = true
 }
 
 terraform {
   backend "azurerm" {
-    key              = "terraform.tfstate"
-    use_azuread_auth = true
-    use_oidc         = true
+    resource_group_name  = "rg-base"
+    storage_account_name = "saandreaswilsonstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_azuread_auth     = true
+    use_oidc             = true
   }
 }
